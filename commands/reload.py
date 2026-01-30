35f2de60597f289fafd0bff199e182224e47abc7
Abr1cOt <thendjuhbrct@gmail.com>
2026-01-30 12:49:44 +0100

    feat: added language support

--- commands/reload.py
+++ commands/reload.py
@@ -1,6 +1,7 @@
 import discord
 from discord import app_commands
 from discord.ext import commands
+import yaml
 import os
 
 owner_id_str = os.getenv('OWNER_USER_ID')
@@ -16,13 +17,26 @@ class Reload(commands.Cog):
 
     @app_commands.command(name="reload", description="Hot-reload the command and event list of the bot")
     async def reload(self, interaction: discord.Interaction):
-        if interaction.user.id == owner_id:    
+        await interaction.response.defer(ephemeral=True)
+        if interaction.user.id == owner_id:
+            guild_id = interaction.guild_id
+            config_path = f'server_configs/{guild_id}/config.yaml'
+
+            try:
+                with open(config_path, 'r') as yaml_file:
+                    config = yaml.safe_load(yaml_file)
+            except FileNotFoundError:
+                print(f"⚠️ Config file not found for guild {guild_id}.")
+                return await interaction.followup.send(f'⚠️ Config file not found for guild {guild_id}.')
+            
+            language = str(config['features'].get('language'))
+
             extensions = []
             folders = ['commands', 'events']
             response = []
 
             client = self.client
-        
+
             for folder in folders:
                 for filename in os.listdir(folder):
                     if filename.endswith('.py'):
@@ -31,18 +45,30 @@ class Reload(commands.Cog):
             for extension in extensions:
                 try:
                     await client.reload_extension(extension)
-                    response.append(f'📦 Loaded extension: {extension}')
+                    if language == "fr":
+                        response.append(f'📦 Extension chargé: {extension}')
+                    else:
+                        response.append(f'📦 Loaded extension: {extension}')
                 except Exception as e:
-                    response.append(f'⚠️ Failed to load extension {extension}: {e}.')
+                    if language == "fr":
+                        response.append(f'⚠️ Erreur lors du chargement de l\'extension {extension}: {e}')
+                    else:
+                        response.append(f'⚠️ Failed to load extension {extension}: {e}.')
 
             try:
                 synced = await client.tree.sync()
-                response.append(f'🌐 Synced {len(synced)} command(s)')
+                if language == "fr":
+                    response.append(f'🌐 {len(synced)} commande(s) synchronisée')
+                else:
+                    response.append(f'🌐 Synced {len(synced)} command(s)')
             except Exception as e:
-                response.append(f"⚠️ Failed to sync commands: {e}")
+                if language == "fr":
+                    response.append(f'⚠️ Erreur lors de la synchronisation des extensions: {e}')
+                else:
+                    response.append(f"⚠️ Failed to sync commands: {e}")
 
             response_message = "\n".join(response)
-            await interaction.response.send_message(f"```ml\n{response_message}\n```")
+            await interaction.followup.send(f"```ml\n{response_message}\n```")
 
         else:
             await interaction.response.send_message("⛔️ You do not have permission to use this command.", ephemeral=True)
