30326fbfa25ac98e68f4b3cbc0785ce893e20ff8
Abr1cOt <antoine@caminade.fr>
2026-01-31 15:34:23 +0100

    feat: add birthday verification task and enhance birthday registration command

--- commands/birthday.py
+++ commands/birthday.py
@@ -10,8 +10,9 @@ class Birthday(commands.Cog):
         self.client = client
     
     @app_commands.command(name="birthday", description="Register or modify your birthday")
-    @app_commands.describe(date="Your birthday in DD/MM/YYYY format")
+    @app_commands.describe(date="Your birthday in DD/MM format")
     async def birthday(self, interaction: discord.Interaction, date: str):
+        await interaction.response.defer(ephemeral=True)
         guild_id = interaction.guild_id
         config_path = f'server_configs/{guild_id}/config.yaml'
         user_data_path = f'server_configs/{guild_id}/{interaction.user.id}.json'
@@ -20,28 +21,39 @@ class Birthday(commands.Cog):
             with open(config_path, 'r') as yaml_file:
                 config = yaml.safe_load(yaml_file)
         except FileNotFoundError:
-            await interaction.response.send_message("⚠️ Config file not found for this server.", ephemeral=True)
-            return
-
+            return await interaction.followup.send("⚠️ Config file not found for this server.", ephemeral=True)
+        
         try:
             with open(user_data_path, 'r') as json_file:
                 user_data = json.load(json_file)
         except FileNotFoundError:
-            print(f"⚠️ User data file not found for user {interaction.author.id} in guild {guild_id}.")
-            return
+            return await interaction.followup.send(f"⚠️ User data file not found for user {interaction.user.id} in guild {guild_id}.")
         
         language = str(config['features'].get('language'))
-        birthday = datetime.strptime(date, "%d/%m/%Y")
+        birthday = date
         birthday_enabled = bool(config['features']['birthday'].get('enabled'))
+
         if birthday_enabled is True:
+            try:
+                datetime.strptime(date, "%d/%m")
+            except ValueError:
+                if language == "fr":
+                    return await interaction.followup.send(f"❌ Date invalide, format JJ/MM attendu (ex: 25/12)", ephemeral=True)
+                else:
+                    return await interaction.followup.send(f"❌ Invalid date, DD/MM format required (ex: 25/12)", ephemeral=True)
+            
             user_data['birthday'] = birthday
             with open(user_data_path, 'w') as json_file:
-                json.dump(user_data, json_file, indent=4)   # À ajouter: confirmation -> 'language'
-            if language == 'fr':
-                await interaction.response.send_message(f"✅ Votre anniversaire a été enregistré comme le {date}.", ephemeral=True)
+                json.dump(user_data, json_file, indent=4)
+            if language == "fr":
+                await interaction.followup.send(f"✅ Votre anniversaire a été enregistré comme le {date}.", ephemeral=True)
             else:
-                await interaction.response.send_message(f"✅ Your birthday has been registered as {date}.", ephemeral=True)
-
+                await interaction.followup.send(f"✅ Your birthday has been registered as {date}.", ephemeral=True)
+        else:
+            if language == "fr":
+                await interaction.followup.send("🧩 Cette fonctionalitée est désactivée sur ce serveur.", ephemeral=True)
+            else:
+                await interaction.followup.send("🧩 This feature is disabled on this server.", ephemeral=True)
 
 async def setup(client):
     await client.add_cog(Birthday(client))
--- events/birthday_verif.py
+++ events/birthday_verif.py
@@ -1,21 +1,25 @@
 import discord
 from discord.ext import commands, tasks
-from datetime import datetime
+import datetime
 import json
 import yaml
 import os
 
-class BirthdayVerification(commands.Cog):
+class BirthdayVerif(commands.Cog):
     def __init__(self, client):
         self.client = client
-
-    @tasks.loop(time=datetime.time(hour=8, minute=0, second=0))
-    async def verify_birthdays(self):
-        for dir in os.listdir('./server_configs'):
-            for users in os.listdir(f'./server_configs/{dir}'):
+        self.verif_birthday.start()
+    
+    def cog_unload(self):
+        self.verif_birthday.cancel()
+    
+    @tasks.loop(time=datetime.time(hour=8, minute=0))
+    async def verif_birthday(self):
+        for dir in os.listdir(f'server_configs'):
+            for users in os.listdir(f'server_configs/{dir}'):
                 if users.endswith('.json'):
-                    user_data_path = f'./server_configs/{dir}/{users}'
-                    config_path = f'./server_configs/{dir}/config.yaml'
+                    user_data_path = f'server_configs/{dir}/{users}'
+                    config_path = f'server_configs/{dir}/config.yaml'
                     today = datetime.now().strftime("%d/%m")
 
                     try:
@@ -24,45 +28,43 @@ class BirthdayVerification(commands.Cog):
                     except FileNotFoundError:
                         print(f"⚠️ Config file not found for guild {dir}.")
                         continue
-
+                    
                     try:
                         with open(user_data_path, 'r') as json_file:
                             user_data = json.load(json_file)
                     except FileNotFoundError:
                         print(f"⚠️ User data file not found: {user_data_path}.")
                         continue
-
+                    
                     birthday_enabled = bool(config['features']['birthday'].get('enabled'))
-                    if birthday_enabled is True and 'birthday' in user_data:
-                        birthday_str = user_data['birthday']
-                        birthday_date = datetime.strptime(birthday_str, "%%d/%m/%Y")
 
-                        if birthday_date.strftime("%d/%m") == today:
+                    if birthday_enabled is True and user_data['birthday'] != "0":
+                        birthday = user_data['birthday']
+
+                        if birthday == today:
                             guild = self.client.get_guild(int(dir))
                             member = guild.get_member(int(users.split('.')[0]))
+
                             if member:
                                 channel_id = int(config['features']['birthday'].get('announcement_channel_id'))
                                 channel = self.client.get_channel(channel_id)
-                                language = config['features'].get('language', 'en')
+                                language = config['features'].get('language')
 
-                                if language == 'fr':
-                                    embed_title = "Joyeux Anniversaire !"
-                                    embed_description = f"🎉🎂 Bon anniversaire à {member.mention} !"
+                                if language == "fr":
+                                    embed_title = f"Joyeux Anniversaire {member.mention} !"
+                                    embed_description = f"🎉🎂 On souhaite un joyeux anniversaire a {member.mention} !"
                                 else:
-                                    embed_title = "Happy Birthday!"
-                                    embed_description = f"🎉🎂 Happy birthday to {member.mention} !"
-
+                                    embed_title = f"Happy Birthday {member.mention} !"
+                                    embed_description = f"🎉🎂 We wish an happy birthday to {member.mention} !"
+                                
                                 embed = discord.Embed(
-                                    title=embed_title,
-                                    description=embed_description,
-                                    color=discord.Color.pink(),
-                                    timestamp=discord.utils.utcnow()
+                                    title = embed_title,
+                                    description = embed_description,
+                                    color = discord.Color.pink(),
+                                    timestamp = discord.utils.utcnow()
                                 )
                                 embed.set_thumbnail(url=member.display_avatar.url)
                                 await channel.send(embed=embed)
-            
-
-            
 
 async def setup(client):
-    await client.add_cog(BirthdayVerification(client))
+    await client.add_cog(BirthdayVerif(client))
